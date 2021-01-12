405e07c9c48ed2e087a2ea307aaa6704b68b9e39
GABRIEL CASAS CORRAL <gabriel.casascorral@servexternos.gruposantander.es>
2021-01-13 00:52:31 +0100

    added data model

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,8 +3,6 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './module/auth.module';
 import { ormconfig } from './orm.config';
 import { PuppeteerModule } from './puppeteer/puppeteer.module';
-import { MongooseModule } from '@nestjs/mongoose';
-import { config } from './config';
 // jhipster-needle-add-entity-module-to-main-import - JHipster will import entity modules here, do not remove
 // jhipster-needle-add-controller-module-to-main-import - JHipster will import controller modules here, do not remove
 // jhipster-needle-add-service-module-to-main-import - JHipster will import service modules here, do not remove
@@ -12,7 +10,6 @@ import { config } from './config';
 @Module({
     imports: [
         TypeOrmModule.forRoot(ormconfig),
-        MongooseModule.forRoot(config.get('mongo.url')),
         AuthModule,
         PuppeteerModule,
     // jhipster-needle-add-entity-module-to-main - JHipster will add entity modules here, do not remove
