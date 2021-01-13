07afb2ec1e0688769aa95835a4142874bd1c52ab
GABRIEL CASAS CORRAL <gabriel.casascorral@servexternos.gruposantander.es>
2021-01-13 01:02:14 +0100

    Mongo service to handle with code repo

--- server/src/puppeteer/code-handler/code-handler.service.ts
+++ server/src/puppeteer/code-handler/code-handler.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CrawConfig, CrawConfigDocument } from 'src/schemas/craw.config.schema';
 
 @Injectable()
 export class CodeHandlerService {
 
-    executeHandler():void{
-        console.log("Execute Handler");
+    private crawConfigModel: Model<CrawConfigDocument>;
+
+    constructor( @InjectModel(CrawConfig.name)  crawConfigModel: Model<CrawConfigDocument>){
+        this.crawConfigModel = crawConfigModel;
+    }
+
+    async obtainFirstNavigation(url:string):Promise<CrawConfigDocument>{
+        return await this.crawConfigModel.findOne({url:url}).exec();
     }
 }
--- server/src/puppeteer/puppeteer.service.ts
+++ server/src/puppeteer/puppeteer.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 import puppeteer  from 'puppeteer-core';
+import { CrawConfig, CrawConfigDocument } from 'src/schemas/craw.config.schema';
 import { CodeHandlerService } from './code-handler/code-handler.service';
 @Injectable()
 export class PuppeteerService {
@@ -9,7 +12,7 @@ export class PuppeteerService {
       this.codeHandlerService = codeHandlerService;
         console.log("here")
         //this.executeCrawling('http://www.visualeconomy.com/');
-        this.codeHandlerService.executeHandler();
+      
     }
 
     public async  executeCrawling(url:string):Promise<void> {
@@ -24,7 +27,9 @@ export class PuppeteerService {
            
 
             //todo: ejecutar navigacion a medida hasta pagina que devuelve la info
-            this.codeHandlerService.executeHandler();
+            
+
+            let info=await this.codeHandlerService.obtainFirstNavigation(url);
              
             await page.on('response', resp  =>  {
               // var header = resp.headers();
